a1fe734c52226156d0ed6b319c46167f3c2aa9b9
ibibo seleye-fubara <ibibofubara@gmail.com>
2018-04-11 16:17:34 +0100

    Add files via upload

---
+++ project1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>learn php</title>
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+</head>
+<body>
+	<?php
+	echo "
+	<h1>hotels.ng internship 4.0 </h1>";
+	date_default_timezone_set('UTC');
+	echo " <h1>The time is  " . date("  h:i:sa </h1> ");
+	?>
+</body>
+</html>
